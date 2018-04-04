4585403cdf89e2a178033baee3498cc546e152ab
kaushik H S <kaushik@kaushiks-MacBook-Pro.local>
2018-04-04 22:48:35 +0530

    adding change to ftp.sh

--- kayako-service-location-master/assets/ftp.sh
+++ kayako-service-location-master/assets/ftp.sh
@@ -1,8 +1,8 @@
 ftp -inpv gbuild.gdev.com << EOF
 user gbuildftp EALhMiuJ8Ry
 cd /ftp/gbuildftp/kayako/location/
-get city.mmdb
-get isp.mmdb
-get conn.mmdb
+get city.mmdb /geo-db/city.mmdb
+get isp.mmdb  /geo-db/isp.mmdb
+get conn.mmdb /geo-db/conn.mmdb
 bye
 EOF
