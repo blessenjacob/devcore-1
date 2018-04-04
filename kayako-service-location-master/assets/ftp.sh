a44c3a60f07b616eaf306ade19c8522ab3b36650
kaushik H S <kaushik@kaushiks-MacBook-Pro.local>
2018-04-04 23:34:50 +0530

    adding binary to ftp.sh

--- kayako-service-location-master/assets/ftp.sh
+++ kayako-service-location-master/assets/ftp.sh
@@ -1,6 +1,7 @@
 ftp -inpv gbuild.gdev.com << EOF
 user gbuildftp EALhMiuJ8Ry
 cd /ftp/gbuildftp/kayako/location/
+bin
 get city.mmdb /geo-db/city.mmdb
 get isp.mmdb  /geo-db/isp.mmdb
 get conn.mmdb /geo-db/conn.mmdb
